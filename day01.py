d2a827d88a37a3aea5b77415a139e2d22e5e7831
bpu500 <bpu500@york.ac.uk>
2023-12-01 10:33:45 +0000

    Day 1 Part 2 incomplete

--- day01.py
+++ day01.py
@@ -1,17 +1,51 @@
-def part1():
+def main(part2):
     total = 0
     with open("./files/day01.txt", "r") as f:
         for line in f:
+            newLine = line
+            if part2:
+                newLine = replace_word_numbers(line)
             numbers = []
-            for char in line:
+            for char in newLine:
                 if char.isdigit():
                     numbers.append(int(char))
             total += numbers[0] * 10 + numbers[-1]
     return total
 
 
-def main():
-    print(part1())
+def replace_word_numbers(line):
+    numbers = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+    newLine = ""
+    pointer = 0
+    end = False
+    while pointer < len(line) and not end:
+        if not line[pointer].isalnum():
+            end = True
+            continue
+        if line[pointer].isdigit():
+            newLine += line[pointer]
+            pointer += 1
+            continue
+        for number_length in range(5, 2, -1):
+            if pointer < len(line) - (number_length - 1):
+                word = line[pointer:pointer + number_length]
+                if word in numbers.keys():
+                    newLine += numbers.get(word)
+                    pointer += number_length - 1
+                    continue
+        pointer += 1
+    return newLine
 
 
-main()
+print(main(False))
+print(main(True))
