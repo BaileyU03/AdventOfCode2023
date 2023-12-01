35712493319f27893e0583ba713b5e28a3e17d4a
bpu500 <bpu500@york.ac.uk>
2023-12-01 11:41:30 +0000

    Day 1 Part 2 improvements - list comprehension

--- day01.py
+++ day01.py
@@ -4,30 +4,28 @@ def main(is_part2):
         for line in f:
             new_line = line
             if is_part2:
-                new_line = replace_word_numbers(line)
-            numbers = []
-            for char in new_line:
-                if char.isdigit():
-                    numbers.append(int(char))
+                numbers = replace_word_numbers(line)
+            else:
+                numbers = [int(char) for char in new_line if char.isdigit()]
             total += numbers[0] * 10 + numbers[-1]
     return total
 
 
 def replace_word_numbers(line):
-    numbers = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
-               "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-    new_line = ""
+    numbersDict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+                   "six": 6, "seven": 7, "eight": 8, "nine": 9}
+    numbers = []
     for i in range(len(line)):
         if line[i].isdigit():
-            new_line += line[i]
+            numbers.append(int(line[i]))
             continue
         for word_length in range(3, 6):
             if i < len(line) - word_length:
                 word = line[i:i + word_length]
-                if word in numbers.keys():
-                    new_line += numbers.get(word)
+                if word in numbersDict.keys():
+                    numbers.append(numbersDict.get(word))
                     break
-    return new_line
+    return numbers
 
 
 print(main(False))
