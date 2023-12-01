8fa56867fcd7eb68d438ff6aa869e8730e1faf38
bpu500 <bpu500@york.ac.uk>
2023-12-01 11:19:54 +0000

    Day 1 Part 2 improvements

--- day01.py
+++ day01.py
@@ -14,35 +14,19 @@ def main(part2):
 
 
 def replace_word_numbers(line):
-    numbers = {
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9"
-    }
+    numbers = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
+               "six": "6", "seven": "7", "eight": "8", "nine": "9"}
     newLine = ""
-    pointer = 0
-    end = False
-    while pointer < len(line) and not end:
-        if not line[pointer].isalnum():
-            end = True
+    for i in range(len(line)):
+        if line[i].isdigit():
+            newLine += line[i]
             continue
-        if line[pointer].isdigit():
-            newLine += line[pointer]
-            pointer += 1
-            continue
-        for number_length in range(5, 2, -1):
-            if pointer < len(line) - (number_length - 1):
-                word = line[pointer:pointer + number_length]
+        for wordLength in range(3, 6):
+            if i < len(line) - wordLength:
+                word = line[i:i+wordLength]
                 if word in numbers.keys():
                     newLine += numbers.get(word)
-                    continue
-        pointer += 1
+                    break
     return newLine
 
 
