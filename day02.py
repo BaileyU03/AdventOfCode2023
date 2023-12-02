e4d9371013d34800d4d8199ec4df3b93bd26173a
bpu500 <bailey.uniacke.03@gmail.com>
2023-12-02 11:58:02 +0000

    Day 2 part 2 complete

--- day02.py
+++ day02.py
@@ -1,7 +1,7 @@
 import re
 
 
-def part01():
+def main(is_part2):
     total = 0
     with open("./files/day02.txt", "r") as f:
         for line in f:
@@ -10,13 +10,13 @@ def part01():
             reds = [int(x.split(" ")[0]) for x in words[1:] if x.split(" ")[1] == "red"]
             greens = [int(x.split(" ")[0]) for x in words[1:] if x.split(" ")[1] == "green"]
             blues = [int(x.split(" ")[0]) for x in words[1:] if x.split(" ")[1] == "blue"]
-            if all(r <= 12 for r in reds) and all(g <= 13 for g in greens) and all(b <= 14 for b in blues):
-                total += game_id
+            if is_part2:
+                total += max(reds) * max(greens) * max(blues)
+            else:
+                if all(r <= 12 for r in reds) and all(g <= 13 for g in greens) and all(b <= 14 for b in blues):
+                    total += game_id
     return total
 
 
-def main():
-    print(part01())
-
-
-main()
+print(main(False))
+print(main(True))
