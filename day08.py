fbc8c490864e6ad4c625280603ee12b15aba2910
bpu500 <bailey.uniacke.03@gmail.com>
2023-12-08 10:07:43 +0000

    Day 8 improvements - simplify based on apparent quirk of data

--- day08.py
+++ day08.py
@@ -11,29 +11,18 @@ def main(is_part2):
     total = 0
     if is_part2:
         current_nodes = [node for node in adj_list.keys() if node[-1] == "A"]
-        node_data = []
+        steps_to_zs = []
         for i, node in enumerate(current_nodes):
-            node_datum = {"first_z": None, "intermediate_z": [], "steps_of_loop": None}
             steps_copy = steps
-            history = [node]
             current_node = node
+            counter = 0
             while current_node[-1] != "Z":
                 step = steps_copy[0]
                 current_node = adj_list[current_node][0] if step == "L" else adj_list[current_node][1]
-                history.append(current_node)
+                counter += 1
                 steps_copy = steps_copy[1:] + step
-            node_datum["first_z"] = len(history) - 1
-            loop_steps = 0
-            while current_node != history[node_datum["first_z"]] or loop_steps == 0:
-                step = steps_copy[0]
-                current_node = adj_list[current_node][0] if step == "L" else adj_list[current_node][1]
-                loop_steps += 1
-                steps_copy = steps_copy[1:] + step
-                if current_node[-1] == "Z":
-                    node_datum["intermediate_z"].append(loop_steps)
-            node_datum["steps_of_loop"] = loop_steps
-            node_data.append(node_datum)
-        return math.lcm(*[node["first_z"] for node in node_data])
+            steps_to_zs.append(counter)
+        return math.lcm(*steps_to_zs)
     else:
         current_node = "AAA"
         while current_node != "ZZZ":
@@ -44,5 +33,5 @@ def main(is_part2):
     return total
 
 
-# print(main(False))
+print(main(False))
 print(main(True))
