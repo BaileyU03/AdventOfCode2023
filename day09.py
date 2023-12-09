a32a6cb58fb97186c414f42c7a5d0c05fcb5cee2
bpu500 <bailey.uniacke.03@gmail.com>
2023-12-09 12:20:08 +0000

    Day 9 improvements

--- day09.py
+++ day09.py
@@ -3,13 +3,11 @@ def main(is_part2):
     with open("./files/day09.txt", "r") as f:
         for line in f:
             sequences.append(list(map(int, line.split())))
-
     total = 0
     for s in sequences:
         s_tree = [s]
         while not all(x == 0 for x in s_tree[-1]):
-            sub = [s_tree[-1][i+1] - s_tree[-1][i] for i in range(len(s_tree[-1]) - 1)]
-            s_tree.append(sub)
+            s_tree.append([s_tree[-1][i+1] - s_tree[-1][i] for i in range(len(s_tree[-1]) - 1)])
         for layer in range(len(s_tree) - 2, -1, -1):
             s_tree[layer] = ([s_tree[layer][0] - s_tree[layer + 1][0]] + s_tree[layer] if is_part2
                              else s_tree[layer] + [s_tree[layer][-1] + s_tree[layer + 1][-1]])
