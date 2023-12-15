fd7addea559ae9fc5da15e0297f3b81613376016
bpu500 <bailey.uniacke.03@gmail.com>
2023-12-15 12:08:25 +0000

    Day 15 improvements - refactor hashing

--- day15.py
+++ day15.py
@@ -2,26 +2,20 @@ def main():
     with open("./files/day15.txt", "r") as f:
         s = f.readline().replace("\n", "").split(",")
     p1_total = 0
+    boxes = [[] for _ in range(256)]
     for word in s:
-        current_value = box_number = 0
-        for char in word:
-            current_value += ord(char)
-            current_value *= 17
-            current_value %= 256
-            if char.isalpha():
-                box_number += ord(char)
-                box_number *= 17
-                box_number %= 256
+        current_value = hash(word)
         p1_total += current_value
-        boxes = [[] for _ in range(256)]
         if "-" in word:
             label = word[:-1]
+            box_number = hash(label)
             if boxes[box_number] != [] and label in [lens[0] for lens in boxes[box_number]]:
                 label_pos = [lens[0] for lens in boxes[box_number]].index(label)
                 boxes[box_number].remove(boxes[box_number][label_pos])
         else:
             label = word[:-2]
-            if boxes[box_number] == []:
+            box_number = hash(label)
+            if not boxes[box_number]:
                 boxes[box_number].append([label, int(word[-1])])
             elif label in [lens[0] for lens in boxes[box_number]]:
                 label_pos = [lens[0] for lens in boxes[box_number]].index(label)
@@ -33,6 +27,15 @@ def main():
         for j, lens in enumerate(box):
             focusing_powers.append((1 + i) * (j + 1) * lens[1])
     return p1_total, sum(focusing_powers)
+
+
+def hash(word):
+    v = 0
+    for char in word:
+        v += ord(char)
+        v *= 17
+        v %= 256
+    return v
 
 
 print(main())
