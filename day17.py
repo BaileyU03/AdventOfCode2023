3735b627e0fb020f78f96096dff61808e2ab96ae
bpu500 <bailey.uniacke.03@gmail.com>
2023-12-17 18:37:57 +0000

    Day 17 part 1 incomplete - off by one?

--- day17.py
+++ day17.py
@@ -1,8 +1,8 @@
 class Node:
-    def __init__(self, n, x, y):
+    def __init__(self, n, x, y, dir):
         self.value = int(n)
         self.dist = -1
-        self.prev_dir = ""
+        self.dir = dir
         self.x = x
         self.y = y
         self.prev = None
@@ -16,85 +16,79 @@ class Node:
 
 
 def main():
-    graph = []
     queue = []
+    height = width = 12
     with open("./files/day17.txt", "r") as f:
         for y, line in enumerate(f):
-            node_line = []
             for x, n in enumerate(line):
                 if n == "\n":
                     continue
-                node = Node(n, x, y)
-                node_line.append(node)
-                queue.append(node)
-            graph.append(node_line)
-    graph[0][0].dist = 0
-    graph[0][0].prev_dir = "e"
-
+                for direction in ["n", "e", "s", "w"]:
+                    for multiplier in range(1, 4):
+                        node = Node(n, x, y, direction * multiplier)
+                        if y == 0 and x == 0 and direction * multiplier == "e":
+                            node.dist = 0
+                        queue.append(node)
+    final_nodes = [node for node in queue if node.x == width - 1 and node.y == height - 1]
     while queue:
         u = min(*queue)
-        print(u.x, u.y, u.dist, u.prev_dir)
         queue.remove(u)
         left_x = right_x = forward_x = u.x
         left_y = right_y = forward_y = u.y
-        if "e" in u.prev_dir:
+        if "e" in u.dir:
             left_y = u.y - 1
             right_y = u.y + 1
             forward_x = u.x + 1
             left = "n"
             right = "s"
-        if "w" in u.prev_dir:
+        if "w" in u.dir:
             left_y = u.y + 1
             right_y = u.y - 1
             forward_x = u.x - 1
             left = "s"
             right = "n"
-        if "n" in u.prev_dir:
+        if "n" in u.dir:
             left_x = u.x - 1
             right_x = u.x + 1
             forward_y = u.y - 1
             left = "w"
             right = "e"
-        if "s" in u.prev_dir:
+        if "s" in u.dir:
             left_x = u.x + 1
             right_x = u.x - 1
             forward_y = u.y + 1
             left = "e"
             right = "w"
-        forward = u.prev_dir + u.prev_dir[0]
+        forward = u.dir + u.dir[0]
 
-        if 0 <= left_x < len(graph[0]) and 0 <= left_y < len(graph):
-            left_node = graph[left_y][left_x]
-            if left_node in queue:
+        if 0 <= left_x < width and 0 <= left_y < height:
+            left_node = [node for node in queue if node.x == left_x and node.y == left_y and node.dir == left]
+            if left_node:
+                left_node = left_node[0]
                 temp = u.dist + left_node.value
-                if left_node.dist == -1 or temp < left_node.dist or (temp == left_node.dist and len(u.prev_dir) > len(left_node.prev_dir)):
+                if left_node.dist == -1 or temp < left_node.dist:
                     left_node.dist = temp
                     left_node.prev = u
-                    left_node.prev_dir = left
-        if 0 <= right_x < len(graph[0]) and 0 <= right_y < len(graph):
-            right_node = graph[right_y][right_x]
-            if right_node in queue:
+        if 0 <= right_x < width and 0 <= right_y < height:
+            right_node = [node for node in queue if node.x == right_x and node.y == right_y and node.dir == right]
+            if right_node:
+                right_node = right_node[0]
                 temp = u.dist + right_node.value
-                if right_node.dist == -1 or temp < right_node.dist or (temp == right_node.dist and len(u.prev_dir) > len(right_node.prev_dir)):
+                if right_node.dist == -1 or temp < right_node.dist:
                     right_node.dist = temp
                     right_node.prev = u
-                    right_node.prev_dir = right
-        if 0 <= forward_x < len(graph[0]) and 0 <= forward_y < len(graph) and len(forward) <= 3:
-            forward_node = graph[forward_y][forward_x]
-            if forward_node in queue:
+        if 0 <= forward_x < width and 0 <= forward_y < height:
+            forward_node = [node for node in queue if node.x == forward_x and node.y == forward_y and node.dir == forward]
+            if forward_node:
+                forward_node = forward_node[0]
                 temp = u.dist + forward_node.value
-                if forward_node.dist == -1 or temp < forward_node.dist or (temp == forward_node.dist and len(u.prev_dir) > len(forward_node.prev_dir)):
+                if forward_node.dist == -1 or temp < forward_node.dist:
                     forward_node.dist = temp
                     forward_node.prev = u
-                    forward_node.prev_dir = forward
 
-        if u == graph[len(graph) - 1][len(graph[0]) - 1]:
-            v = u
-            while v:
-                print(v.x, v.y)
-                v = v.prev
-            return u.dist
-
+        if u.x == width - 1 and u.y == height - 1 and len([node for node in queue if node.x == width - 1 and node.y == height - 1]) == 6:
+            v = min(*final_nodes)
+            return v.dist
 
 print(main())
 
